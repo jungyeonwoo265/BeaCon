d67cbaeb44b5e8bafd046a5b398fc5822317e739
jungyeonwoo265 <oqpwe7@naver.com>
2023-01-09 15:21:22 +0900

    2301091521

--- mian.py
+++ mian.py
@@ -2,12 +2,14 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 
-form_class = uic.loadUiType("main.ui")[0]
+form_class = uic.loadUiType("user.ui")[0]
 
-class WindowClass(QMainWindow, form_class) :
+
+class WindowClass(QMainWindow, form_class):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
