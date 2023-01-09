f978334f98f78db11bd775195e9131403d385ffa
jungyeonwoo265 <oqpwe7@naver.com>
2023-01-09 18:41:43 +0900

    2301091841

--- mian.py
+++ mian.py
@@ -2,7 +2,7 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 
-form_class = uic.loadUiType("user.ui")[0]
+form_class = uic.loadUiType("main.ui")[0]
 
 
 class WindowClass(QMainWindow, form_class):
