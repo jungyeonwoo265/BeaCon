990d59447831977714684f6dc19644750a4ca916
jungyeonwoo265 <oqpwe7@naver.com>
2023-01-10 11:26:48 +0900

    2301101126

--- manager.py
+++ manager.py
@@ -5,14 +5,20 @@ from PyQt5 import uic
 form_class = uic.loadUiType("manager.ui")[0]
 
 
-class WindowClass(QMainWindow, form_class):
+class Manager(QMainWindow, form_class):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
 
+        self.name = str
+
+
+    def set_information(self, name):
+        self.name = name
+        print(self.name)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    myWindow = WindowClass()
+    myWindow = Manager()
     myWindow.show()
     app.exec_()
